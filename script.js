e99dd11646ae9eb68a44cd6b4b9216c68d26a987
Manikothakota-git <143675898+Manikothakota-git@users.noreply.github.com>
2023-12-03 19:42:01 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// script.js
+function viewDetails(productId) {
+    // Handle viewing product details, e.g., redirect to a details page
+    console.log(`View details for product ${productId}`);
+}
